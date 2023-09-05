b4f143a4dc35061fcaf0156a4aa541d0853ec28e
PhilipMuturi <ranktech@gmail.com>
2023-09-06 00:02:44 +0300

    Volume of a cylinder

--- Cylinder.c
+++ Cylinder.c
@@ -1,1 +1,23 @@
-Volume of a cylinder
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // Declare variables to store radius and height
+    float radius, height;
+
+    // Prompt the user for input
+    printf("Enter the radius of the cylinder: ");
+    scanf("%f", &radius);
+
+    printf("Enter the height of the cylinder: ");
+    scanf("%f", &height);
+
+    // Calculate the volume of the cylinder
+    float volume = M_PI * pow(radius, 2) * height;
+
+    // Display the result
+    printf("The volume of the cylinder is: %.2f cubic units\n", volume);
+
+    return 0;
+}
+
